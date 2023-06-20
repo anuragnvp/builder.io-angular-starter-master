87328c8f3c009da2f26f44fad887de2f31220bdd
anurag gupta <anurag.gupta@convergesolution.com>
2023-06-21 03:07:50 +0530

    new added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BuilderModule } from '@builder.io/angular';
 
-import { AppComponent, CustomThing } from './app.component';
+import { AppComponent, CustomThing, CustomThingNew } from './app.component';
 import { FooComponent } from './foo.component';
 import { CustomThingChildren } from './with-children';
 import { HeaderComponent } from './header/header.component';
 import { BenefitsComponent } from './benefits/benefits.component';
 @NgModule({
-  declarations: [AppComponent, FooComponent, CustomThing, CustomThingChildren,HeaderComponent,BenefitsComponent],
+  declarations: [AppComponent, FooComponent, CustomThing, CustomThingChildren,HeaderComponent,BenefitsComponent,CustomThingNew],
   entryComponents: [CustomThing, CustomThingChildren],
   imports: [
     BrowserModule,
